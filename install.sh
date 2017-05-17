7b1dbcbb977d0dc32a383a39a7e62b4dbbb9a876
ducky-hong <pacifings@gmail.com>
2017-05-17 22:19:40 +0900

    install tpm if it is not installed

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+
+set -e
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 ln -sf $(pwd)/vimrc ~/.vimrc
 ln -sf $(pwd)/tmux.conf ~/.tmux.conf
